fef6c6e4afd55b342f56d88cf5b8d41467bf1209
ankitchawla3123 <ankitchawla3123@gmail.com>
2025-08-11 21:48:28 +0530

    lineup schema

--- src/models/lineups.models.js
+++ src/models/lineups.models.js
@@ -1,12 +1,32 @@
 import mongoose, { Schema } from "mongoose";
 
-const lineupsSchema = new Schema({
-  homeformation: {
-    type: String,
-    required: true,
-  },
-  awayformation: {
-    type: String,
-    required: true,
-  },
+const playerSchema = new Schema({
+  id: { type: Number },
+  name: { type: String },
+  number: { type: Number },
+  pos: { type: String },
+  grid: { type: String, default: null },
 });
+
+
+const teamLineupSchema = new Schema({
+  teamId: { type: Number, required: true },
+  name: { type: String, required: true },
+  formation: { type: String, required: true },
+  coach: {
+    type: String,
+  },
+  startXI: [playerSchema],
+  substitutes: [playerSchema],
+});
+
+const lineupsSchema = new Schema(
+  {
+    fixtureId: { type: Number, required: true },
+    homeTeam: teamLineupSchema,
+    awayTeam: teamLineupSchema,
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Lineup", lineupsSchema);
